8bb060af2d13649e6808754dba9f74a0f9a87191
suryasn35 <36407992+suryasn35@users.noreply.github.com>
2018-02-23 17:18:15 +0530

    Add files via upload

---
+++ 2-4.py
@@ -0,0 +1,5 @@
+m=int(input("Enter a number:"))
+n=int(input("Enter a number:"))
+for i in range(m+1,n+1):
+    if(i%2!=0):
+        print(i)
---
+++ 2-5.py
@@ -0,0 +1,5 @@
+m=int(input("enter the lower limit range "))
+n=int(input("enter the upper limit range"))
+for i in range(m,n+1):
+	if(i%2==0):
+		print(i)
